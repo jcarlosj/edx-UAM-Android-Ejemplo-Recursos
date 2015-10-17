030e23d352660b865699942465043a93c458bc9e
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-16 22:27:30 -0500

    Cambia implementación anterior desde XML a Java
     - Elimina los parámetros y los valores de los componentes de tipo
       "Button" relacionados con: el texto que contiene, el color del texto
       y sus dimensiones en el fichero "activity_main.xml"
     - Agrega funcionalidad a la clase "ActivityMain" para adicionar las
       características descritas en el punto anterior desde el código java.

--- app/src/main/java/co/jce/edx/recursosdelsistema/MainActivity.java
+++ app/src/main/java/co/jce/edx/recursosdelsistema/MainActivity.java
@@ -2,12 +2,38 @@ package co.jce.edx.recursosdelsistema;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button,
+                   button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
+
+    private void init() {
+        //-> Accedemos a los componentes del "Activity"
+        button = ( Button ) findViewById( R .id .button );
+        button2 = ( Button ) findViewById( R .id .button2 );
+
+        //-> Asignamos las cadenas de texto a los botones extrayendolas del fichero "strings.xml"
+        button .setText(getResources().getString(R.string.label_grande));
+        button2 .setText( getResources() .getString( R .string .label_reducido ) );
+
+        //-> Asignamos un tamaño (dimensión) a las cadenas de los botones en el fichero "dimens.xml"
+        button .setTextSize( getResources() .getDimension( R .dimen .tipo_grande ) );
+        button2 .setTextSize( getResources() .getDimension( R .dimen .tipo_reducido ) );
+
+        //-> Asignamos un color a las cadenas de texto de los botones en el fichero "colors.xml"
+        //   El método getColor() quedará obsoleto apartir de la API 23.
+        //   En su lugar debe usarse así: getColor ( int, Theme )
+        button .setTextColor( getResources() .getColor( R .color .colorVerde ) );
+        button2 .setTextColor( getResources() .getColor( R. color .colorRojo ) );
+    }
+
 }
