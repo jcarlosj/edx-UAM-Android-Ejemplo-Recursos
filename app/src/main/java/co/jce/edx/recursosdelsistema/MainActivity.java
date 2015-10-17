71b975bd658099e21612416d8e1cd4f3a35a1a86
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-16 20:57:51 -0500

    Inicializa el repositorio para el proyecto

---
+++ app/src/main/java/co/jce/edx/recursosdelsistema/MainActivity.java
@@ -0,0 +1,13 @@
+package co.jce.edx.recursosdelsistema;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
